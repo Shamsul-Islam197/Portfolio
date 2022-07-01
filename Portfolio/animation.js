88b0a60c3099841d366d06d941dcdfff51e1d6cc
Shamsul-Islam197 <mdshamsulislam197@gmail.com>
2022-07-01 16:24:08 +0600

    Portfolio

---
+++ Portfolio/animation.js
@@ -0,0 +1,9 @@
+
+    var typed = new Typed(".typing", {
+        strings: [ "Developer", "Programmer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    
